eeccaedcf43298835a1ce447988cb3a076ac6b76
jatinaga <jatin.agrawal_cs18@gla.ac.in>
2020-03-28 19:47:24 +0530

    Execution class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,24 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(1);
+        obj.insert(20);
+        obj.insert(50);
+        obj.insert(22);
+        obj.insert(9);
+        obj.insert(5);
+        obj.insert(60);
+        System.out.println("Left child nodes are: ");
+        obj.printLeftNode(obj.root);
+        System.out.println(" No of nodes that dosn't have a left child node are : " + obj.getCount());
+
+
+    }
+
 }

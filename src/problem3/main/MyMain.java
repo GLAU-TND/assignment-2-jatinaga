18fd02a96079fbb3cf8c8d7c15eb3445b7acc8e3
jatinaga <jatin.agrawal_cs18@gla.ac.in>
2020-03-28 20:12:25 +0530

    create the instance variable

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,20 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("yash", 60);
+        obj.insert("Rohit", 1);
+        obj.insert("kan", 20);
+        obj.insert("vishwarya", 2);
+        obj.insert("deshya", 9);
+        System.out.println("Students according to their roll number are: ");
+        obj.peep();
+    }
 }
---
+++ src/problem4/node/Node.java
@@ -0,0 +1,12 @@
+/*
+ *   Created by IntelliJ IDEA.
+ *   User: HPW
+ *   Date: 28-03-2020
+ *   Time: 20:11
+ */
+package problem4.node;
+
+public class Node {
+    int data;
+    Node next;
+}

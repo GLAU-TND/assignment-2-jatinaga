6a560567cbb52969ac8f88950eebe54803c18a25
jatinaga <jatin.agrawal_cs18@gla.ac.in>
2020-03-28 20:23:46 +0530

    declaring the getter and setter methods

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -12,4 +12,20 @@ import problem5.student.Student;
 public class Node {
     Student student;
     Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
